6481ffb844efa2711acba7c9a0d5eaa0a4e63291
Vivek Pandey <snapstar78@gmail.com>
2024-05-19 22:16:16 +0530

    Color fix for tech-stack-badge

--- src/app/widgets/details/experience/tech-stack-badge/index.tsx
+++ src/app/widgets/details/experience/tech-stack-badge/index.tsx
@@ -1,7 +1,7 @@
 const TechStackBadge = ({ techStack }: { techStack: string }) => {
   return (
     <li className="mr-1.5 mt-2">
-      <div className="flex items-center rounded-full bg-teal-400/40 px-3 py-1 text-xs font-medium leading-5 text-[#112A46] dark:text-[#BCC5D2]">
+      <div className="flex items-center rounded-full bg-teal-400/40 dark:teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-[#112A46] dark:text-slate-200">
         {techStack}
       </div>
     </li>
