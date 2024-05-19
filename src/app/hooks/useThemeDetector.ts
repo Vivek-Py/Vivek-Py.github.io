8346ef4757a7af8a1845320e77f222d205df54fc
Vivek Pandey <snapstar78@gmail.com>
2024-05-19 22:23:52 +0530

    Theme client window error fix

--- src/app/hooks/useThemeDetector.ts
+++ src/app/hooks/useThemeDetector.ts
@@ -7,10 +7,7 @@ interface CustomEvent {
 }
 
 const useThemeDetector = () => {
-  const getCurrentTheme = () =>
-    window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-  const [isDarkTheme, setIsDarkTheme] = useState(getCurrentTheme());
+  const [isDarkTheme, setIsDarkTheme] = useState(true);
 
   const mqListener = (e: CustomEvent) => {
     setIsDarkTheme(e.matches);
