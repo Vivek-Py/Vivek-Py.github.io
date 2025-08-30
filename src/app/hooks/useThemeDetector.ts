5b19c0d8e154ff54a345cb83edb1db445713bf0b
Vivek Pandey <snapstar78@gmail.com>
2025-08-30 23:48:53 +0530

    Hard coding theme and updated profile

--- src/app/hooks/useThemeDetector.ts
+++ src/app/hooks/useThemeDetector.ts
@@ -7,16 +7,16 @@ interface CustomEvent {
 }
 
 const useThemeDetector = (): String => {
-  const [isDarkTheme, setIsDarkTheme] = useState("dark");
-  const mqListener = (e: CustomEvent) => {
-    setIsDarkTheme(e.matches ? "dark" : "light");
-  };
-  useEffect(() => {
-    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
-    mqListener(darkThemeMq);
-    darkThemeMq.addEventListener("change", mqListener);
-    return () => darkThemeMq.removeEventListener("change", mqListener);
-  }, []);
+  const [isDarkTheme, setIsDarkTheme] = useState("light");
+  // const mqListener = (e: CustomEvent) => {
+  //   setIsDarkTheme(e.matches ? "dark" : "light");
+  // };
+  // useEffect(() => {
+  //   const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+  //   mqListener(darkThemeMq);
+  //   darkThemeMq.addEventListener("change", mqListener);
+  //   return () => darkThemeMq.removeEventListener("change", mqListener);
+  // }, []);
   return isDarkTheme;
 };
 
